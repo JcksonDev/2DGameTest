5d635ff98c23796ec2b8991de6778612814b6b47
JcksonDev <lunaagamezz@gmail.com>
2021-08-28 23:59:57 -0700

    Added display method

---
+++ src/dev/jcksondev/gametest/Display.java
@@ -0,0 +1,27 @@
+package dev.jcksondev.gametest;
+
+import javax.swing.*;
+
+public class Display {
+    private JFrame frame;
+
+    private String title;
+    private int width, height;
+
+    Display(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay() {
+        frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
--- src/dev/jcksondev/gametest/Launcher.java
+++ src/dev/jcksondev/gametest/Launcher.java
@@ -2,6 +2,6 @@ package dev.jcksondev.gametest;
 
 public class Launcher {
     public static void main(String[] args) {
-
+        new Display("2D Game", 500, 500);
     }
 }
