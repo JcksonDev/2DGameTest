fa21ad46f994ee1f7a28f4d2af75cd8ebe59eda7
JcksonDev <lunaagamezz@gmail.com>
2021-08-29 00:54:07 -0700

    Added buffers and graphics

--- src/dev/jcksondev/gametest/Game.java
+++ src/dev/jcksondev/gametest/Game.java
@@ -2,16 +2,75 @@ package dev.jcksondev.gametest;
 
 import dev.jcksondev.gametest.display.Display;
 
-public class Game {
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game implements Runnable{
     private Display display;
     private String title;
     private int width, height;
+
+    private boolean running = false;
+    private Thread thread;
+
+    private BufferStrategy bs;
+    private Graphics g;
 
     public Game(String title, int width, int height) {
         this.title = title;
         this.width = width;
         this.height = height;
+    }
 
+    private void init() {
         display = new Display(title, width, height);
+    }
+
+    private void tick() {}
+
+    private void render() {
+        bs = display.getCanvas().getBufferStrategy();
+        if (bs == null) {
+            display.getCanvas().createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+        // Start drawing
+
+        g.fillRect(0, 0, width, height);
+
+        // End drawing
+
+        bs.show();
+        g.dispose();
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        while (running) {
+            tick();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        if (running) return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        if (!running) return;
+        running = false;
+        try {
+            thread.join();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/dev/jcksondev/gametest/Launcher.java
+++ src/dev/jcksondev/gametest/Launcher.java
@@ -2,6 +2,7 @@ package dev.jcksondev.gametest;
 
 public class Launcher {
     public static void main(String[] args) {
-        new Game("2D Game", 500, 500);
+        Game game = new Game("2D Game", 500, 500);
+        game.start();
     }
 }
